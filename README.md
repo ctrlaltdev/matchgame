c257be7db46436cd83484983d5966457cd1104f3
Yorick <yorick@ctrlaltdev.xyz>
2018-06-12 18:21:50 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# wordmatch
-A small javascript library for generating word (or what you want) match game
+
---
+++ src/matchgame.js
@@ -0,0 +1,9 @@
+/**
+ *Create a word match game
+ * @class wordMatch
+ */
+class matchGame {
+
+}
+
+export default matchGame
